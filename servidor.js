f4ebccb58365190541461efd1c8d1a02b57fec1d
menychamu-99 <mannuel_99@hotmail.com>
2021-11-22 15:56:47 -0700

    subiendo el proyecto nodejs 404

---
+++ servidor.js
@@ -0,0 +1,52 @@
+var https = require('http');
+var fs = require('fs');
+var path = require('path');
+
+https.createServer(function(request, response){
+    console.log('request', request.url);
+
+    var filePath ='.'+request.url;
+    if(filePath == './'){
+        filePath = './index.html';
+    }
+
+var extname = String(path.extname(filePath)).toLowerCase();
+var contentType = 'text/html';
+var mimeTypes = {
+    '.html' : 'text/html',
+    '.js' : 'text/javascript' ,
+    '.css' : 'text/css' ,
+    '.json' : 'aplication/json' ,
+    '.png' : 'image/png' ,
+    '.jpg' : 'image/jpg' ,
+    '.gif' : 'image/gif' ,
+    '.wav' : 'audio/wav' ,
+    '.mp4' : 'video/mp4' ,
+    '.woff' : 'aplication/font-woff' ,
+    '.ttf' : 'aplication/font-ttf' ,
+    '.eot' : 'aplication/vnd.ms-fontobject' ,
+    '.otf' : 'aplication/font-otf' ,
+    '.svg' : 'aplication/image/svg+xml' ,
+};
+
+contentType = mimeTypes[extname] || 'application/octet-stream';
+
+fs.readFile(filePath, function(error, content){
+    if(error) {
+        if(error.code == 'ENOENT'){
+            fs.readFile('./404.html', function(error, content){
+                response.writeHead(200, {'Content-Type': contentType });
+                response.end(content, 'utf-8');
+            });
+        }else {
+        response.writeHead(500);
+        response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+        response.end();
+    }
+    }else{
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
+    }
+});
+}).listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
